20c3a8d97ae040d8382378a8064adee3ed0883b5
dfedorov <desuk2@gmail.com>
2025-06-08 16:27:36 +0300

    add req

--- simple-python-webserver/app.py
+++ simple-python-webserver/app.py
@@ -1,10 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 
 app = Flask(__name__)
 
 @app.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "running"}), 200
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    # Example of standard Prometheus metrics format
+    metrics_data = (
+        "# HELP python_app_requests_total Total number of requests\n"
+        "# TYPE python_app_requests_total counter\n"
+        "python_app_requests_total{endpoint=\"/status\"} 1\n"
+    )
+    return Response(metrics_data, mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
